7da31207f87d833a2ed0780fde369e895cd8f34b
shaanjijoe <shaanjijoe2@gmail.com>
2023-12-18 18:53:53 +0530

    Sockets shifted to seperate file from index.js

--- server/index.js
+++ server/index.js
@@ -2,28 +2,20 @@ const express = require('express')
 const app = express()
 const http = require('http');
 const server = http.createServer(app);
-const io = require('socket.io')(server, {
-    cors: {
-      origin: '*',
-      methods: ["GET", "POST"],
-    }
-  });
 const cors = require('cors') //For accessing api within own url for localhost access
-const jwt = require('jsonwebtoken')
-const {getProfileForUsername, getMessagesAndProfileImage, insertMessage} = require('./chatData')
 const sequelize = require('./orm')
 const authenticationrouter = require('./authentication');
-const { getAllUsers } = require('./users');
+const configureSockets = require('./socketing')
 
 require('./models/Associations')
 require("dotenv").config();
 app.use(cors({
-    origin: '*',
-  }));
-  
+  origin: '*',
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/',authenticationrouter);  //Auth Apis
+app.use('/', authenticationrouter);  //Auth Apis
 
 
 
@@ -36,87 +28,8 @@ sequelize.sync()
     console.error('Error synchronizing database:', error);
   });
 
-const authSocketMiddleware = (socket, next) => {
-  
-    const AuthToken = socket.handshake.auth.authToken; 
-    jwt.verify(AuthToken, process.env.ACCESS_TOKEN_KEY, (err, user) => {
-        if (err) return  next(new Error("NOT AUTHORIZED"));
-        // console.log(user);
-        socket.user = user;
-        next();
-    })
-    
-  };
-  
-  const usernameToSocketId = {};
-  const socketIdToUsername = {};
-
-
-io.use((socket, next) => {
-    authSocketMiddleware(socket, next);
-  });
-
-
-
-io.on('connection', (socket) => {
-    // console.log('user connected');
-    // console.log(socket.handshake.auth.authToken);
-    console.log(socket.user.username, ' connected.');
-
-    usernameToSocketId[socket.user.username] = socket.id;
-    socketIdToUsername[socket.id] = socket.user.username;
-
-socket.on('initiate', async () => {
-        // Handle the 'initiate' event and send necessary data back to the sender
-        const responseData = {
-          profileImage: await getProfileForUsername(socket.user.username),
-          allUsers: await getAllUsers(),
-        };
-        socket.emit('initiate-response', responseData);
-      });
-
-
-socket.on('getcontent', async () => {
-        // Handle the 'initiate' event and send necessary data back to the sender
-        const text = await getMessagesAndProfileImage(socket.user.username);
-        const texts = {
-          texts: text,
-        }
-        console.log(socket.user.username, ' content sent.')
-        socket.emit('getcontent-response', texts);
-      });
-
-socket.on('message-input', async (data) => {
-        if(data.messageType === 'text')
-          await insertMessage(data);
-        else
-        {
-          console.log(data);
-          return;
-        }
-
-
-        if(usernameToSocketId[data.sender]!=null){
-          io.to(usernameToSocketId[data.sender]).emit('message-update');
-        }
-
-        if(usernameToSocketId[data.receiver]!=null){
-          io.to(usernameToSocketId[data.receiver]).emit('message-update');
-        }
-        
-        
-      });
-
-    
-      
-    // Handle disconnection
-socket.on('disconnect', () => {
-      console.log(socket.user.username,' disconnected');
-      const disconnectedUsername = socketIdToUsername[socket.id];
-      usernameToSocketId[disconnectedUsername] = null;
-      socketIdToUsername[socket.id] = null;
-    });
-  });
+// Configure sockets and pass the http server instance
+configureSockets(server);
 
 
 const PORT = process.env.PORT || 4000;
--- server/socketing.js
+++ server/socketing.js
@@ -1,25 +1,100 @@
-// socketing.js
-const socketIo = require('socket.io');
+// socket.js
+const socketIO = require('socket.io');
+const cors = require('cors'); //For accessing api within own url for localhost access
+const jwt = require('jsonwebtoken');
+const { getProfileForUsername, getMessagesAndProfileImage, insertMessage } = require('./chatData');
+const { getAllUsers } = require('./users');
 
-function setupSocket(server) {
-  const io = socketIo(server);
+function configureSockets(httpServer) {
+  const io = socketIO(httpServer, {
+    cors: {
+      origin: '*',
+      methods: ["GET", "POST"],
+    }
+  });
 
-  // Socket.IO connection handling
+
+  const authSocketMiddleware = (socket, next) => {
+
+    const AuthToken = socket.handshake.auth.authToken;
+    jwt.verify(AuthToken, process.env.ACCESS_TOKEN_KEY, (err, user) => {
+      if (err) return next(new Error("NOT AUTHORIZED"));
+      // console.log(user);
+      socket.user = user;
+      next();
+    })
+
+  };
+
+  const usernameToSocketId = {};
+  const socketIdToUsername = {};
+
+
+  io.use((socket, next) => {
+    authSocketMiddleware(socket, next);
+  });
+
+
+
   io.on('connection', (socket) => {
-    console.log('A user connected');
+    // console.log('user connected');
+    // console.log(socket.handshake.auth.authToken);
+    console.log(socket.user.username, ' connected.');
 
-    // Handle events from the client
-    socket.on('exampleEvent', (data) => {
-      console.log('Received data from client:', data);
+    usernameToSocketId[socket.user.username] = socket.id;
+    socketIdToUsername[socket.id] = socket.user.username;
+
+    socket.on('initiate', async () => {
+      // Handle the 'initiate' event and send necessary data back to the sender
+      const responseData = {
+        profileImage: await getProfileForUsername(socket.user.username),
+        allUsers: await getAllUsers(),
+      };
+      socket.emit('initiate-response', responseData);
     });
+
+
+    socket.on('getcontent', async () => {
+      // Handle the 'initiate' event and send necessary data back to the sender
+      const text = await getMessagesAndProfileImage(socket.user.username);
+      const texts = {
+        texts: text,
+      }
+      console.log(socket.user.username, ' content sent.')
+      socket.emit('getcontent-response', texts);
+    });
+
+    socket.on('message-input', async (data) => {
+      if (data.messageType === 'text')
+        await insertMessage(data);
+      else {
+        console.log(data);
+        return;
+      }
+
+
+      if (usernameToSocketId[data.sender] != null) {
+        io.to(usernameToSocketId[data.sender]).emit('message-update');
+      }
+
+      if (usernameToSocketId[data.receiver] != null) {
+        io.to(usernameToSocketId[data.receiver]).emit('message-update');
+      }
+
+
+    });
+
+
 
     // Handle disconnection
     socket.on('disconnect', () => {
-      console.log('User disconnected');
+      console.log(socket.user.username, ' disconnected');
+      const disconnectedUsername = socketIdToUsername[socket.id];
+      usernameToSocketId[disconnectedUsername] = null;
+      socketIdToUsername[socket.id] = null;
     });
   });
 
-  return io;
 }
 
-module.exports = setupSocket;
+module.exports = configureSockets;
